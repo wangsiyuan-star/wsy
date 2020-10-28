a8b51cf32446f0d7ca8d44740a7257a3bd1c8d37
wangsiyuan-star <73217542+wangsiyuan-star@users.noreply.github.com>
2020-10-28 19:46:02 +0800

    Update FIbonacci_3.py

--- Fibonacci/FIbonacci_3.py
+++ Fibonacci/FIbonacci_3.py
@@ -1,39 +1,26 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[16]:
-
-
-#code:utf_8
+#code:utf-8
 
 """王思媛 20201028
    用迭代器方法实现斐波那契数列"""
 
-class Fibonacci():
-    def __init__(self,n):
+class Fibonacci():    #创建斐波那契迭代器
+    def __init__(self,n):    #初始化
         self.n = n
         self.a = 1
         self.b = 0
         self.count = 0
         
-    def __iter__(self):
+    def __iter__(self):    #定义iter
         return self
     
-    def __next__(self): 
+    def __next__(self):     #定义next
         while self.count < self.n:
                 self.a,self.b = self.b,self.a+self.b
                 self.count += 1
                 return self.a
-                
-        
 
-        
-
-
-# In[17]:
-
-
-fib = Fibonacci(10)
-for i in range(10):
+fib = Fibonacci(10)    #调用斐波那契迭代器
+for i in range(10):    #遍历迭代器
     print(next(fib),end = " ")
 
